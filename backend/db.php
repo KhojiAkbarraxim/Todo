a4c868174fd05829f6745d613ddf6924c18cf295
KhojiAkbarraxim <xraximovx020304@gmail.com>
2026-05-26 22:54:08 +0500

    q removed 2

--- backend/db.php
+++ backend/db.php
@@ -2,6 +2,7 @@
 $pdo = new PDO('sqlite:/app/todos.db');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+
 $pdo->exec("
 CREATE TABLE IF NOT EXISTS todos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
